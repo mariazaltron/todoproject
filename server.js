1afb19c1a1b63cd105a96f4444308f01e8e89bfb
mariazaltron <mariaemzaltron@gmail.com>
2022-09-01 22:12:06 +0200

    server adjusts

--- server.js
+++ server.js
@@ -6,21 +6,20 @@ const User = require("./models").user;
 
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 app.post("/echo", (req, res) => {
   res.json(req.body);
 });
 
-//display all the users
+//display all the users - FUNCIONANDO
 
 app.get("/users", async (req, res) => {
   const allUsers = await User.findAll();
-  res.send(allUsers)
+  res.send(allUsers);
 });
 
-
-// Create a new user account
+// // Create a new user account
 app.post("/users", async (req, res, next) => {
   try {
     const email = req.body.email;
@@ -35,10 +34,21 @@ app.post("/users", async (req, res, next) => {
   }
 });
 
-//GET requests to /users/:userId
+//POST new user - FUNCIONANDO
+
+// app.post("/users", async (req, res, next) => {
+//   try {
+//     const newUser = await User.create(req.body);
+//      res.status(201).send(newUser);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+//GET requests to /users/:userId - FUNCIONANDO
 
 app.get("/users/:id", async (req, res) => {
-  const specificUser = await User.findByPk(req.params.id)
+  const specificUser = await User.findByPk(req.params.id);
   if (specificUser) {
     res.send(specificUser);
   } else {
